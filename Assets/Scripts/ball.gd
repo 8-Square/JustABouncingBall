03e19ba7cfa23b399b2fe1b93d7a1b0aa0fa5b55
4_Square <104998118+8-Square@users.noreply.github.com>
2025-10-27 16:35:40 +1000

    Basic Game Loop Working

--- Assets/Scripts/ball.gd
+++ Assets/Scripts/ball.gd
@@ -1,12 +1,31 @@
 extends CharacterBody2D
 
-const SPEED: float = 400
+var SPEED: float = 350
+var collision_count: float = 0 
 
 func _ready() -> void:
 	initialize()
 
 func _physics_process(delta: float) -> void:
-	move_and_slide()
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		collision_count += 1
+		print("COLLISION NUMBER IS ON ", collision_count)
+		velocity = velocity.bounce(collision.get_normal())
+		
+	if collision_count >= 32:
+		SPEED = 770
+	if collision_count >= 25:
+		SPEED = 650
+	elif collision_count >= 17:
+		SPEED = 550
+	elif collision_count >= 10:
+		SPEED = 450
+	else:
+		SPEED = 350
+	
+	velocity = velocity.normalized() * SPEED
+
 	
 func initialize():
 	var extra_offset = 0.0 if randf() < 0.5 else PI
@@ -17,4 +36,5 @@ func initialize():
 
 func _on_ball_exited() -> void:
 	await get_tree().create_timer(0.75).timeout
+	collision_count = 0
 	initialize()
