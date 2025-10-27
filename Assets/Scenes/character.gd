306629e15306d8d45dabb6de264955136098b8fc
4_Square <104998118+8-Square@users.noreply.github.com>
2025-10-27 12:59:21 +1000

    Trying to make a background

--- Assets/Scenes/character.gd
+++ Assets/Scenes/character.gd
@@ -3,15 +3,7 @@ extends CharacterBody2D
 @export var player_id: int
 
 const SPEED: float = 400
-#
-#func _input(event: InputEvent) -> void:
-	#if event.is_action_pressed('down-1'):
-		#velocity = Vector2(0, SPEED)
-	#if event.is_action_pressed('up-%d' % player_id):
-		#velocity = Vector2(0, -SPEED)
-	#if event.is_action_released('up-%d' % player_id) or event.is_action_released('down-%d' % player_id):
-		#velocity = Vector2.ZERO
-#
+
 func _process(delta: float) -> void:
 	#AHHHHHHHHHH THIS TOOK SO LONG
 	if Input.is_action_pressed('down-%d' % player_id):
